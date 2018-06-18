682544f3d0f227c2d0792bc8043f98b907af53b6
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-18 21:07:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,7 @@ def merge_data(keys, data)
   merge = []
   keys.each do |element|
     data.first.map do |key, value|
-      if element.values[0] == k 
+      if element.value[0] == k 
         merge << element.merge(value)
       end
     end
