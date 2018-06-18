4389f774f731724618858742a9936bdbc22c9288
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-18 23:17:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -78,13 +78,13 @@ array = []
   schools.each do |name, hash|
   hash.each do |location_key, location|
     if new_hash[location].nil?
-    array << name
-    new_hash[location] = array
-    array = []
-    elsif new_hash[location]
-    new_hash[location] << name
-    array = []
-    end
+      array << name
+      new_hash[location] = array
+      array = []
+      elsif new_hash[location]
+      new_hash[location] << name
+      array = []
+      end
     end
   end
 new_hash
