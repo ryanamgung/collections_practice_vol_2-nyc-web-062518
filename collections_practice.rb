5d42e7f60621edc5118d368d7d81d7f1479c8d78
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-18 23:15:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -65,7 +65,7 @@ def organize_schools(array)
   schools = []
   array.each do |school, element|
     if schools.include?(element[:location])
-      schools
+      schools[element[:location]] << 
       schools[element[:location]] = school
     else
       schools[element[:location]] << school
